676a9d35366ea5435e5bf06fee89ade3b9f53932
tanghonggang1 <tanghonggang1@jd.com>
2019-05-09 20:33:50 +0800

    init project

---
+++ sbia-adaptor/sbia-adaptor-service/src/main/java/com/tm/sbia/adaptor/service/SbiaAdaptorService.java
@@ -0,0 +1,16 @@
+package com.tm.sbia.adaptor.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbiaAdaptorService {
+    /**
+     * 负责启动引导应用程序
+     *
+     * @param args 参数
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SbiaAdaptorService.class, args);
+    }
+}
